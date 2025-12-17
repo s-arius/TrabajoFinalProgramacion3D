d558971ec1085289979eed30ab55b35af1dc300e
vivgim <vivesgimenos@gmail.com>
2025-12-17 15:03:35 +0100

    UploadBelen_009 Cuadro
    Animacion del cuadro rompiendose, falta interacción con personaje.

---
+++ Assets/Scripts/Piso_03/CuadroRoto.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class CuadroRoto : MonoBehaviour
+{
+    GameObject cuadro;
+    GameObject cuadroRoto;
+    Renderer[] cuadroRenderer;
+
+    float timer = 0f;
+    bool waiting = false;
+
+    bool isBlinking = false;
+    float delay = 1.5f;
+    float visibleTime = 2f;
+    float blink = 0.5f;
+    float blinkTimer = 0;
+
+    float offTime = 0.3f;
+    bool isOff = false;
+
+    public float appearSpeed =9f; 
+    bool isMoving = false;
+
+    void Start()
+    {
+        cuadro = transform.GetChild(0).gameObject;
+        cuadroRoto = transform.GetChild(1).gameObject;
+        cuadroRoto.SetActive(false);
+        cuadroRenderer = cuadroRoto.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer r in cuadroRenderer)
+        {
+            r.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        breakPainting();
+        if (isMoving)
+        {
+            // move forward in local z-axis
+            cuadroRoto.transform.Translate(Vector3.forward * appearSpeed * Time.deltaTime);
+
+            // optional: stop after moving a certain distance
+            // e.g., move 1 unit forward
+            if (cuadroRoto.transform.localPosition.z >= 6f)
+            {
+                isMoving = false;
+            }
+        }
+    }
+
+    void SetRenderers(bool value)
+    {
+        foreach (Renderer r in cuadroRenderer)
+        {
+            r.enabled = value;
+        }
+    }
+
+
+    void breakPainting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !waiting)
+        {
+            waiting = true;
+            timer = 0f;
+        }
+
+        if (!waiting) return;
+
+            timer += Time.deltaTime;
+
+            if (timer >= delay && !cuadroRoto.activeSelf)
+            {
+                cuadro.SetActive(false);
+                cuadroRoto.SetActive(true);
+                SetRenderers(true);
+                isMoving = true;
+
+        }
+
+            if (timer >= delay + visibleTime && !isBlinking)
+        {
+            isBlinking = true;
+            blinkTimer = 0f;
+        }
+
+        if (isBlinking)
+        {
+            blinkTimer += Time.deltaTime;
+
+            if (!isOff && blinkTimer >= blink)
+            {
+                SetRenderers(false); // OFF
+                isOff = true;
+                blinkTimer = 0f;
+            }
+            else if (isOff && blinkTimer >= offTime)
+            {
+                SetRenderers(true); // ON
+                isOff = false;
+                blinkTimer = 0f;
+            }
+
+            if (timer >= delay + visibleTime + 3f)
+            {
+                SetRenderers(false);
+                waiting = false;
+                isBlinking = false;
+                isOff = false;
+                blinkTimer = 0f;
+            }
+        }
+
+    }
+}
+
